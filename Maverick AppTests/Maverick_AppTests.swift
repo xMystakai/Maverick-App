4c040cfa87514b57e5931313466bfaa544f3220b
Kai Comer <Mystakai@maed-3.local>
2017-04-12 10:03:21 -0600

    Initial Commit

---
+++ Maverick AppTests/Maverick_AppTests.swift
@@ -0,0 +1,36 @@
+//
+//  Maverick_AppTests.swift
+//  Maverick AppTests
+//
+//  Created by Kai Comer on 4/12/17.
+//  Copyright © 2017 Kai Comer. All rights reserved.
+//
+
+import XCTest
+@testable import Maverick_App
+
+class Maverick_AppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
