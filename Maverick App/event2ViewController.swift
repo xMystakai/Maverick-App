4290973fe8196454b34d31b644c5c8f5346344c2
allenwelch <allenwelch725@gmail.com>
2017-04-21 09:14:29 -0600

    yes

---
+++ Maverick App/event2ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  event2ViewController.swift
+//  Maverick App
+//
+//  Created by allen welch on 4/21/17.
+//  Copyright © 2017 Kai Comer. All rights reserved.
+//
+
+import UIKit
+
+class event2ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
