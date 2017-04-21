6249269ba6bb015f2a16e23b5d110a272777a7f7
allenwelch <allenwelch725@gmail.com>
2017-04-21 10:37:35 -0600

    q

--- Maverick App/event2ViewController.swift
+++ Maverick App/event2ViewController.swift
@@ -8,17 +8,33 @@
 
 import UIKit
 
-class event2ViewController: UIViewController {
+class event2ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "commentsID", for: indexPath) as! Comments
+        cell.emailsLBL.text = "cat@cat.cat"
+        cell.actualComment.text = "i am commenting about this topic cause it is a topic"
+        return cell
+    }
 
     @IBAction func dismissButton() {
         dismiss(animated: true, completion: nil)
     }
 
+}
+class Comments : UITableViewCell{
+    @IBOutlet weak var emailsLBL: UILabel!
+    @IBOutlet weak var actualComment: UITextView!
+    
 }
